639223c44757c86de4835c960ff6e55751efa05a
Alejandro <acuellarh@gmail.com>
2019-03-19 11:45:43 -0500

    Sinatra r1 Saludo

--- solution.rb
+++ solution.rb
@@ -2,9 +2,9 @@ require 'sinatra'
 
 get '/' do
 
-  unless params[:nombre]   #Si no recibe en el navegador el valor de nombre para el hash params, haga
+  unless params[:nombre]
     "<h1>Hola desconocido!</h1>"
-  else                     #Significa que recibío el valor de la clave :nombre en el hash params
+  else
     "<h1>Hola #{params[:nombre]}!</h1>"
   end
 
